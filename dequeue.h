bb5b55e618f8f3333dc2ce4804d16594c6467be3
fuzaila <43177938+fuzaila@users.noreply.github.com>
2018-10-21 19:37:47 +0530

    Create dequeue.h

---
+++ dequeue.h
@@ -0,0 +1,9 @@
+#include<stdbool.h>
+void setsize(int a);
+bool isempty();
+bool isfull(int size);
+void insert_rear(int item);
+void delete_front();
+void insert_front(int item);
+void delete_rear();
+void print();
